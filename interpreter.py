7e1598fcc5c9638e2ec5ef19fcf0d2a3542cde40
Christopher <christopher.rocks@bell.net>
2020-08-04 14:22:34 -0400

    Added iFrame support

--- interpreter.py
+++ interpreter.py
@@ -122,5 +122,8 @@ INTERPRETER = {
     "SETDATE": commands.date_input,
     "EXTRACT": commands.extract_html,
     "SWITCHNEWWINDOW": commands.switch_to_newly_opened_window,
-    "SWITCHFIRSTWINDOW": commands.switch_to_original_window
+    "SWITCHFIRSTWINDOW": commands.switch_to_original_window,
+    "TOIFRAME": commands.switch_to_iframe,
+    "FROMIFRAME": commands.switch_from_iframe,
+    "READFILE": commands.read_file
 }
